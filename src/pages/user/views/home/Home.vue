d8e72908485cc20096d38ff3bf87d1fbc144ce0a
quyans@sina.com <quyans@sina.com>
2019-06-23 08:24:15 +0800

    init

---
+++ src/pages/user/views/home/Home.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="home" :style="note"  >
+        <div   class="sub_home" :style="sub_note" style="">
+                <MyTitle></MyTitle>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+    import Icon from "@/components/Icon";
+    import {getGroupInfo} from "@/resource/group";
+    import MyTitle from "./components/MyTitle"
+    export default {
+        name: 'home',
+        components: {
+            Icon,
+            MyTitle
+        },
+        created() {
+        },
+        data() {
+            return {
+                BASE_URL: process.env.BASE_URL,
+                searchId: '',
+                isScanningQRCode: false,
+                note:{
+                    backgroundImage: "url(" + require("../../../../image/User/bk1.png") + ")",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: '100% 100%',
+                },
+                sub_note:{
+                    backgroundImage: "url(" + require("../../../../image/User/bk2.jpg") + ")",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: '100% 100%',
+                }
+
+            }
+        },
+        methods: {
+
+
+
+        }
+    }
+</script>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    html,body{
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
+<style lang="scss" scoped>
+    .home {
+        section.main {
+            padding: 0 1em;
+        }
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        /*.icon_scan {*/
+            /*width: 14px;*/
+            /*height: 14px;*/
+            /*vertical-align: bottom;*/
+        /*}*/
+    }
+    .sub_home{
+        position: relative;
+        margin: 0 auto;
+        top: 40px;
+        box-shadow:5px 5px 30px darkgrey;
+        min-height: 400px;
+        width: 80%;
+    }
+</style>
+<style lang="scss">
+    .el-dialog {
+        width: 100% !important;
+        max-width: 450px;
+    }
+    #app {
+        height: 100%;
+    }
+</style>
---
+++ src/pages/user/views/home/components/MyTitle.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="my-title" :style="note">
+        <div class="button_group" style="color: white">
+            <i class="el-icon-user"></i>
+            <el-button type="text" style="color: #eadcff" >登录</el-button>
+            <el-button type="text" style="color: #eadcff">注册</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyTitle",
+
+        data() {
+            return {
+                note: {
+                    backgroundImage: "url(" + require("../../../../../image/User/bk3.png") + ")",
+                    backgroundRepeat: "no-repeat",
+                    // height: '100%',
+                    backgroundSize: '100% 100%'
+                },
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .my-title{
+        width: 100%;
+        height: 300px;
+        
+    }
+    .button_group{
+        color: white;
+        width: 150px;
+        padding-left: 50px;
+        padding-top: 20px;
+    }
+</style>
